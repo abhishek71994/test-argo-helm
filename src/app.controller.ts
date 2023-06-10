6d7e121dd3ee9864dc58381c1c4e72f394f4324f
abhishek.prasad <abhishek71994@gmail.com>
2023-06-10 20:47:38 +0800

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('/health')
+  healthCheck(): string {
+    return 'OK';
+  }
+}
